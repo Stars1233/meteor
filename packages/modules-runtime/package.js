5d5bb6ed9052b9a5e7847dcd233312a53b247ef2
Nacho Codoñer <igcogi@gmail.com>
2025-09-09 13:55:53 +0200

    don't bump modules packages as ecmascript is enough to fix publish issues

--- packages/modules-runtime/package.js
+++ packages/modules-runtime/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "modules-runtime",
-  version: '0.13.3-beta340.5',
+  version: '0.13.2',
   summary: "CommonJS module system",
   git: "https://github.com/benjamn/install",
   documentation: "README.md"
