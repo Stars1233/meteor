16c5ba40cbf68e58165b34c32cb915a2e3e7bf39
Nacho Codoñer <igcogi@gmail.com>
2025-11-03 17:44:29 +0100

    bump rspack npm package to 0.0.61 to include the latest changes

--- packages/rspack/lib/constants.js
+++ packages/rspack/lib/constants.js
@@ -5,7 +5,7 @@
 
 export const DEFAULT_RSPACK_VERSION = '1.6.0';
 
-export const DEFAULT_METEOR_RSPACK_VERSION = '0.0.60';
+export const DEFAULT_METEOR_RSPACK_VERSION = '0.0.61';
 
 export const DEFAULT_METEOR_RSPACK_REACT_HMR_VERSION = '1.4.3';
 
