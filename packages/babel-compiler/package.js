199885e459fd88b31926f4bad000d38f79d90b11
Nacho Codoñer <igcogi@gmail.com>
2025-11-27 13:53:42 +0100

    Update @meteorjs/swc-core to version 1.15.3

--- packages/babel-compiler/package.js
+++ packages/babel-compiler/package.js
@@ -9,7 +9,7 @@ Npm.depends({
   '@meteorjs/babel': '7.20.1',
   'json5': '2.2.3',
   'semver': '7.6.3',
-  "@meteorjs/swc-core": "1.13.5",
+  "@meteorjs/swc-core": "1.15.3",
 });
 
 Package.onUse(function (api) {
